8f3ca1b9615757090a8e2a0f58733ba1590f9a42
Maxim Surin <surin.maks@yandex.ru>
2025-05-07 14:28:55 +0300

    заготовки под тесты

--- hw2/tests/test_viewdata.cpp
+++ hw2/tests/test_viewdata.cpp
@@ -8,41 +8,43 @@
 BOOST_AUTO_TEST_SUITE(ViewDataTest) // Начало набора тестов
 
     BOOST_AUTO_TEST_CASE(test_View1) {
-        // hw2::IP ip1;
-        // std::istringstream iss1("192.168.1.1");
-        // ip1.ReadData(iss1);
+        hw2::IP ip1, ip2;
+        std::istringstream iss1("192.168.1.2");
+        std::istringstream iss2("192.168.1.1");
+        ip1.ReadData(iss1);
+        ip2.ReadData(iss2);
 
         // BOOST_CHECK_EQUAL(ip1.getIP(), "192.168.1.1");
     }
 
     BOOST_AUTO_TEST_CASE(test_View2) {
-        // hw2::IP ip1, ip2;
-        // std::istringstream iss1("192.168.1.1");
-        // std::istringstream iss2("192.168.1.2");
-        // ip1.ReadData(iss1);
-        // ip2.ReadData(iss2);
+        hw2::IP ip1, ip2;
+        std::istringstream iss1("192.168.1.2");
+        std::istringstream iss2("192.168.1.1");
+        ip1.ReadData(iss1);
+        ip2.ReadData(iss2);
 
         // BOOST_CHECK(ip1 < ip2); // Проверяем, что ip1 < ip2
         // BOOST_CHECK(!(ip2 < ip1)); // Проверяем обратное
     }
 
     BOOST_AUTO_TEST_CASE(test_View3) {
-        // hw2::IP ip1, ip2;
-        // std::istringstream iss1("192.168.1.1");
-        // std::istringstream iss2("192.168.1.2");
-        // ip1.ReadData(iss1);
-        // ip2.ReadData(iss2);
+        hw2::IP ip1, ip2;
+        std::istringstream iss1("192.168.1.2");
+        std::istringstream iss2("192.168.1.1");
+        ip1.ReadData(iss1);
+        ip2.ReadData(iss2);
 
         // BOOST_CHECK(ip1 < ip2); // Проверяем, что ip1 < ip2
         // BOOST_CHECK(!(ip2 < ip1)); // Проверяем обратное
     }
 
     BOOST_AUTO_TEST_CASE(test_View4) {
-        // hw2::IP ip1, ip2;
-        // std::istringstream iss1("192.168.1.1");
-        // std::istringstream iss2("192.168.1.2");
-        // ip1.ReadData(iss1);
-        // ip2.ReadData(iss2);
+        hw2::IP ip1, ip2;
+        std::istringstream iss1("192.168.1.2");
+        std::istringstream iss2("192.168.1.1");
+        ip1.ReadData(iss1);
+        ip2.ReadData(iss2);
 
         // BOOST_CHECK(ip1 < ip2); // Проверяем, что ip1 < ip2
         // BOOST_CHECK(!(ip2 < ip1)); // Проверяем обратное
