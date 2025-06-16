1a3aa6baba5ba098aaceed7fb87a4e6732573124
Maxim Surin <surin.maks@yandex.ru>
2025-06-16 21:55:45 +0300

    Добавлены задания

--- hw03/main.cpp
+++ hw03/main.cpp
@@ -1,24 +1,47 @@
 #include <iostream>
+#include <map>
 #include "MyList.hpp"
+
+namespace usefull{
+    int factorial(int n)
+    {
+        int res = 1;
+        int i;
+        for (i = 2; i <= n; i++)
+            res *= i;
+        return res;
+    }
+}
 
 int main(int, char**){
     std::cout << "Hello, from hw03!\n";
+
+    size_t MAX_VALUE{10};
+
+    // - создание экземпляра std::map<int, int>
+    std::map<int, int> std_map;
+
+    // - заполнение 10 элементами, где ключ - это число от 0 до 9, а значение - факториал ключа
+    for(size_t i = 0; i < MAX_VALUE; ++i){
+        std_map.emplace(i, usefull::factorial(i));
+    }
+
+    // - создание экземпляра std::map<int, int> с новым аллокатором, ограниченным 10 элементами
+    // - заполнение 10 элементами, где ключ - это число от 0 до 9, а значение - факториал ключа
+    // - вывод на экран всех значений (ключ и значение разделены пробелом) хранящихся в контейнере
+    for(const auto& [key, value] : std_map){
+        std::cout << key << ' ' << value << '\n';
+    }
+
+    // - создание экземпляра своего контейнера для хранения значений типа int
     hw03::MyList<int> mylist;
-    mylist.push_back(1);
-    mylist.push_back(3);
-    mylist.printList();
-    mylist.insertIntoBegin(5);
-    mylist.printList();
-    mylist.insertIntoEnd(7);
-    mylist.printList();
-    mylist.insertIntoPosition(2,7);
-    mylist.printList();
-    std::cout << "Тест оператора []: "  << mylist[1] << "\n";
-    mylist.eraseInBegin();
-    mylist.printList();
-    mylist.eraseInEnd();
-    mylist.printList();
-    mylist.eraseInPosition(0);
-    // mylist.eraseInPosition(0);
+
+    // - заполнение 10 элементами от 0 до 9
+    for(size_t i = 0; i < MAX_VALUE; ++i){
+        mylist.push_back(i);
+    }
+    // - создание экземпляра своего контейнера для хранения значений типа int с новым аллокатором, ограниченным 10 элементами
+    // - заполнение 10 элементами от 0 до 9
+    // - вывод на экран всех значений, хранящихся в контейнере
     mylist.printList();
 }
